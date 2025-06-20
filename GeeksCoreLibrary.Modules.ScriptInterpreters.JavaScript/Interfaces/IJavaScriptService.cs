ce65ebd6f6482a8b2b5da43aea420f851ece027c
Hans <35447467+HansJuice@users.noreply.github.com>
2025-06-20 13:19:23 +0200

    Initial commit

---
+++ GeeksCoreLibrary.Modules.ScriptInterpreters.JavaScript/Interfaces/IJavaScriptService.cs
@@ -0,0 +1,64 @@
+﻿namespace GeeksCoreLibrary.Modules.ScriptInterpreters.JavaScript.Interfaces;
+
+/// <summary>
+/// Interface for a JavaScript interpreter service.
+/// </summary>
+public interface IJavaScriptService : IDisposable
+{
+    /// <summary>
+    /// Set a value in the JavaScript engine.
+    /// </summary>
+    /// <param name="name">The name of the global object.</param>
+    /// <param name="value">The value of the global object.</param>
+    void SetValue(string name, object value);
+
+    /// <summary>
+    /// Get a value from the JavaScript engine.
+    /// </summary>
+    /// <param name="name">The name of the JavaScript object to get the value of.</param>
+    /// <returns>The value of the object.</returns>
+    object GetValue(string name);
+
+    /// <summary>
+    /// Get a value from the JavaScript engine.
+    /// </summary>
+    /// <param name="name">The name of the JavaScript object to get the value of.</param>
+    /// <returns>The value of the object.</returns>
+    T GetValue<T>(string name);
+
+    /// <summary>
+    /// Execute a script statement. Use this to, for example, create a function which can later be invoked using the <see cref="Invoke{T}(string, object[])"/> method.
+    /// </summary>
+    /// <param name="script">The script to execute.</param>
+    void Execute(string script);
+
+    /// <summary>
+    /// Evaluate a script statement and return the result.
+    /// </summary>
+    /// <param name="script">The script to execute.</param>
+    /// <returns>An <see cref="Object"/> containing the result of the script.</returns>
+    object Evaluate(string script);
+
+    /// <summary>
+    /// Evaluate a script statement and return the result. If the result is null or does not implement the <see cref="IConvertible"/> interface, it will return the default value of <see cref="T"/>.
+    /// </summary>
+    /// <param name="script">The script to execute.</param>
+    /// <returns>A <see cref="T"/> containing the result of the script.</returns>
+    T Evaluate<T>(string script);
+
+    /// <summary>
+    /// Invoke a function with the given name and arguments.
+    /// </summary>
+    /// <param name="functionName">The name of the function.</param>
+    /// <param name="arguments">The arguments to pass to the function.</param>
+    /// <returns>An <see cref="Object"/> representing the result of the function. The returned value will return null if the function wasn't found or if the function returned undefined or null.</returns>
+    object Invoke(string functionName, params object[] arguments);
+
+    /// <summary>
+    /// Invoke a function with the given name and arguments.
+    /// </summary>
+    /// <param name="functionName">The name of the function.</param>
+    /// <param name="arguments">The arguments to pass to the function.</param>
+    /// <returns>A <see cref="T"/> representing the result of the function. The returned value will return null if the function wasn't found or if the function returned undefined or null.</returns>
+    T Invoke<T>(string functionName, params object[] arguments);
+}
---
+++ GeeksCoreLibrary.Modules.ScriptInterpreters.JavaScript/Services/JavaScriptService.cs
@@ -0,0 +1,148 @@
+﻿using GeeksCoreLibrary.Core.DependencyInjection.Interfaces;
+using GeeksCoreLibrary.Core.Extensions;
+using GeeksCoreLibrary.Modules.ScriptInterpreters.JavaScript.Interfaces;
+using Jint;
+using Microsoft.Extensions.Logging;
+
+namespace GeeksCoreLibrary.Modules.ScriptInterpreters.JavaScript.Services;
+
+/// <summary>
+/// A service for interpreting JavaScript.
+/// </summary>
+public class JavaScriptService : IJavaScriptService, ITransientService
+{
+    private readonly ILogger<JavaScriptService> logger;
+
+    private Engine engine;
+
+    public JavaScriptService(ILogger<JavaScriptService> logger)
+    {
+        this.logger = logger;
+
+        engine = new Engine(new Options
+        {
+            Strict = true,
+            Host =
+            {
+                StringCompilationAllowed = false
+            }
+        });
+
+        // Add GCL methods to the state.
+        var gcl = new
+        {
+            ConvertToSeo = new Func<string, string>(StringExtensions.ConvertToSeo),
+            EncryptWithAes = new Func<string, string, bool, bool, string>(StringExtensions.EncryptWithAes),
+            DecryptWithAes = new Func<string, string, bool, int, bool, string>(StringExtensions.DecryptWithAes),
+            ToSha512 = new Func<string, string>(StringExtensions.ToSha512Simple),
+            ToSha512ForPasswords = new Func<string, byte[], string>(StringExtensions.ToSha512ForPasswords)
+        };
+
+        // Expose the GCL methods to the engine.
+        engine.SetValue("GCL", gcl);
+    }
+
+    /// <inheritdoc />
+    public void SetValue(string name, object value)
+    {
+        engine.SetValue(name, value);
+    }
+
+    /// <inheritdoc />
+    public object GetValue(string name)
+    {
+        return engine.GetValue(name).ToObject();
+    }
+
+    /// <inheritdoc />
+    public T GetValue<T>(string name)
+    {
+        return (T) Convert.ChangeType(engine.GetValue(name).ToObject(), typeof(T));
+    }
+
+    /// <inheritdoc />
+    public void Execute(string script)
+    {
+        engine.Execute(script);
+    }
+
+    /// <inheritdoc />
+    public object Evaluate(string script)
+    {
+        return engine.Evaluate(script).ToObject();
+    }
+
+    /// <inheritdoc />
+    public T Evaluate<T>(string script)
+    {
+        object result = null;
+
+        try
+        {
+            result = Evaluate(script);
+            if (result is Newtonsoft.Json.Linq.JToken jToken)
+            {
+                return jToken.ToObject<T>();
+            }
+
+            return result is not IConvertible ? default : (T) Convert.ChangeType(result, typeof(T));
+        }
+        catch (Exception exception) when (exception is InvalidCastException or FormatException)
+        {
+            logger.LogError(exception, "An error occurred while casting the result of a JavaScript snippet. Tried to cast '{Result}' to type '{GenericType}'", result ?? "null", typeof(T));
+            return default;
+        }
+        catch (Exception exception)
+        {
+            logger.LogError(exception, "An error occurred while executing a JavaScript snippet.");
+            return default;
+        }
+    }
+
+    /// <inheritdoc />
+    public object Invoke(string functionName, params object[] arguments)
+    {
+        return engine.Invoke(functionName, arguments).ToObject();
+    }
+
+    /// <inheritdoc />
+    public T Invoke<T>(string functionName, params object[] arguments)
+    {
+        object result = null;
+
+        try
+        {
+            result = Invoke(functionName, arguments);
+            return result is not IConvertible ? default : (T) Convert.ChangeType(result, typeof(T));
+        }
+        catch (Exception exception) when (exception is InvalidCastException or FormatException)
+        {
+            logger.LogError(exception, "An error occurred while casting the result of a JavaScript function. Tried to cast '{Result}' to type '{GenericType}'", result ?? "null", typeof(T));
+            return default;
+        }
+        catch (Exception exception)
+        {
+            logger.LogError(exception, "An error occurred while executing a JavaScript function.");
+            return default;
+        }
+    }
+
+    #region Disposing
+
+    /// <inheritdoc />
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposing) return;
+
+        engine?.Dispose();
+        engine = null;
+    }
+
+    #endregion
+}
